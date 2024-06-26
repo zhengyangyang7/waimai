4f1db0eb392463c97a72a364362108cfbe938946
chenyang <1907615595@qq.com>
2024-06-26 09:52:34 +0800

    Initial commit

---
+++ src/test/java/com/veliona/elemebackend/ElemebackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.veliona.elemebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElemebackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
